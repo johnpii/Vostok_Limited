a158f4bbae84c6f548eb6419f81a916ebc91d487
johnpii <roman.shubin2001@mail.ru>
2025-07-15 21:18:20 +0400

    init

---
+++ Восток_Тест_Запросы_Шубин.sql
@@ -0,0 +1,40 @@
+USE Vostok
+
+-- Напишите запрос, выбирающий из первой таблицы все данные по контейнерам в формате JSON не используя встроенную функцию
+
+SELECT
+    '[' + STRING_AGG(
+        '{' +
+        '"Id":"' + CAST(Id AS NVARCHAR(36)) + '",' +
+        '"Number":' + CAST(Number AS NVARCHAR(10)) + ',' +
+        '"Type":"' + [Type] + '",' +
+        '"Length":' + CAST([Length] AS NVARCHAR(10)) + ',' +
+        '"Width":' + CAST(Width AS NVARCHAR(10)) + ',' +
+        '"Height":' + CAST(Height AS NVARCHAR(10)) + ',' +
+        '"Weight":' + CAST([Weight] AS NVARCHAR(10)) + ',' +
+        '"IsEmpty":' + CASE IsEmpty WHEN 1 THEN 'true' ELSE 'false' END + ',' +
+        '"ArrivalDate":"' + CONVERT(NVARCHAR(25), ArrivalDate, 121) + '"' +
+        '}',
+        ','
+    ) + ']' AS JsonDataA
+FROM Containers
+
+-- Напишите запрос, выбирающий из второй таблицы все данные по операциям для определенного контейнера в формате JSON не используя встроенную функцию
+
+DECLARE @ContainerId UNIQUEIDENTIFIER = '1DA7A395-7FAE-48CC-8798-C6CA24CC7601'; -- Замените на нужный Id контейнера
+
+SELECT
+    '[' + STRING_AGG(
+        '{' +
+        '"Id":"' + CAST(Id AS NVARCHAR(36)) + '",' +
+        '"ContainerID":"' + CAST(ContainerID AS NVARCHAR(36)) + '",' +
+        '"OperationStartDate":"' + CONVERT(NVARCHAR(25), OperationStartDate, 121) + '",' +
+        '"OperationEndDate":"' + CONVERT(NVARCHAR(25), OperationEndDate, 121) + '",' +
+        '"OperationType":"' + OperationType + '",' +
+        '"OperatorFullName":"' + OperatorFullName + '",' +
+        '"InspectionPlace":"' + InspectionPlace + '"' +
+        '}',
+        ','
+    ) + ']' AS JsonDataB
+FROM Operations
+WHERE ContainerID = @ContainerId;
---
+++ Восток_Тест_Табл_Шубин.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE Vostok;
+GO
+
+USE Vostok;
+GO
+
+-- Напишите скрипты по созданию двух таблиц
+
+-- Таблица контейнеров
+CREATE TABLE Containers (
+    Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+    Number INT NOT NULL,
+    [Type] NVARCHAR(20) NOT NULL,
+    [Length] DECIMAL(10, 2) NOT NULL,
+    Width DECIMAL(10, 2) NOT NULL,
+    Height DECIMAL(10, 2) NOT NULL,
+    [Weight] DECIMAL(10, 2) NOT NULL,
+    IsEmpty BIT NOT NULL,
+    ArrivalDate DATETIME NOT NULL
+);
+GO
+
+-- Таблица операций
+CREATE TABLE Operations (
+    Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+    ContainerID UNIQUEIDENTIFIER NOT NULL,
+    OperationStartDate DATETIME NOT NULL,
+    OperationEndDate DATETIME NOT NULL,
+    OperationType NVARCHAR(50) NOT NULL,
+    OperatorFullName NVARCHAR(100) NOT NULL,
+    InspectionPlace NVARCHAR(100) NOT NULL,
+    FOREIGN KEY (ContainerID) REFERENCES Containers(ID) ON DELETE CASCADE
+);
+GO
